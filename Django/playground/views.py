a89706a527bb7fa667199ea8669ff0ecc69782f7
Amandeep Singh <amandeep87557@gmail.com>
2026-01-25 13:19:24 +0530

    First view function

--- Django/playground/views.py
+++ Django/playground/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def say_hello(request):
+    # Pull data from db
+    # Transfrom data
+    # Send emails
+    return HttpResponse("Hello World")
