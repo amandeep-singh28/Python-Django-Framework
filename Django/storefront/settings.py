58a8baa03d8c8eb882ae1ae84c73cc0794b9b569
Amandeep Singh <amandeep87557@gmail.com>
2026-01-25 12:39:50 +0530

    Removed Session app as it is of no use

--- Django/storefront/settings.py
+++ Django/storefront/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    # Deleted the session app
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
