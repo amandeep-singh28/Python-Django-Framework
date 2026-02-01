7b63a5b646949f747a3d59fed4c7311ef5fac744
Amandeep Singh <amandeep87557@gmail.com>
2026-02-01 13:59:21 +0530

    Query Set Concept in Django

--- Django/playground/views.py
+++ Django/playground/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 # Create your views here.
 
-def calculate():
-    x = 1
-    y = 2
-    return x
+# def calculate():
+#     x = 1
+#     y = 2
+#     return x
 
 def say_hello(request):
-    x = calculate()
+    # x = calculate()
     # x = 1
     # y = 2
     # Pull data from db
@@ -16,4 +17,11 @@ def say_hello(request):
     # Send emails
     # return HttpResponse("Hello World")
     # return render(request, 'hello.html')
+    query_set = Product.objects.all() # For pulling out all the objects in the products table
+    # for data in query_set:
+    #     print(data)
+
+    # list(query_set)
+
+    # query_set[0:5]
     return render(request, 'hello.html', {'name' : 'Amandeep'})
