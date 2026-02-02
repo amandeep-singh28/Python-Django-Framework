1bb1bef983ff5597710480ac38c0dd68962ca215
Amandeep Singh <amandeep87557@gmail.com>
2026-02-02 20:55:47 +0530

    Implementation of F objects

--- Django/playground/views.py
+++ Django/playground/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q # Q means query(It returns query expression or a piece of code that produce a value)
+from django.db.models import Q, F # Q means query(It returns query expression or a piece of code that produce a value)
 from store.models import Product
 
 def say_hello(request):
-    queryset = Product.objects.filter(Q(inventory__lt = 100) & ~Q(unit_price__lt = 700.00))
+    queryset = Product.objects.filter(inventory = F('collection__id'))
 
     return render(request, 'hello.html', {'name' : 'Amandeep', 'products' : list(queryset)})
