17d29e59cdbe6a0bfa2a4fb84f74e4046f2ce12a
Amandeep Singh <amandeep87557@gmail.com>
2026-02-03 20:35:53 +0530

    Very IMP(Implementation of .only() and .values() and verifying it on debug tool bar

--- Django/playground/views.py
+++ Django/playground/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F # Q means query(It returns query expression or a piece of code that produce a value)
-from store.models import Product
+from store.models import Product, OrderItem
 
 def say_hello(request):
-    queryset = Product.objects.filter(inventory = F('collection__id'))
+    # queryset = Product.objects.values('id', 'title')
+    queryset = Product.objects.only('id', 'title')
+
 
     return render(request, 'hello.html', {'name' : 'Amandeep', 'products' : list(queryset)})
+    
