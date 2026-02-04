1fd858643adabaa6e98129614a5b7bb5121cc8a2
Amandeep Singh <amandeep87557@gmail.com>
2026-02-04 09:42:24 +0530

    Prefetch Related

--- Django/playground/views.py
+++ Django/playground/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F # Q means query(It returns query expression or a piece of code that produce a value)
-from store.models import Product, OrderItem
+from store.models import Product, OrderItem, Order
 
 def say_hello(request):
-    queryset = Product.objects.select_related('collection').all()
+    queryset = Order.objects.select_related('customer').order_by('placed_at')[:5]
 
 
-    return render(request, 'hello.html', {'name' : 'Amandeep', 'products' : list(queryset)})
+    return render(request, 'hello.html', {'name' : 'Amandeep', 'orders' : list(queryset)})
     
