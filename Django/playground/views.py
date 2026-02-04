5ac5c46b1e314748853fe97ab79ffd71614ebdd1
Amandeep Singh <amandeep87557@gmail.com>
2026-02-04 10:11:59 +0530

    Annotating Objects(Creating new columns for our existing table)

--- Django/playground/views.py
+++ Django/playground/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F # Q means query(It returns query expression or a piece of code that produce a value)
 from django.db.models.aggregates import Count, Min, Max, Sum, Avg
-from store.models import Product, OrderItem, Order
+from django.db.models import Value
+from store.models import Product, OrderItem, Order, Customer
 
 def say_hello(request):
-    # result = Product.objects.aggregate(Count('id'))
-    result = Product.objects.aggregate(count = Count('id'), min_price = (Min('unit_price')))
+    
+    queryset = Customer.objects.annotate(new_id = F('id') + 1)
 
-
-    return render(request, 'hello.html', {'name' : 'Amandeep', 'result' : result})
+    return render(request, 'hello.html', {'name' : 'Amandeep', 'result' : list(queryset)})
     
