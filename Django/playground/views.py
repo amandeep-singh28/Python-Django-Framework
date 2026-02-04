45145e3f2e6749bc39498245b80af5581a565798
Amandeep Singh <amandeep87557@gmail.com>
2026-02-04 09:56:46 +0530

    Grouping Data (Agrregate Functions

--- Django/playground/views.py
+++ Django/playground/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F # Q means query(It returns query expression or a piece of code that produce a value)
+from django.db.models.aggregates import Count, Min, Max, Sum, Avg
 from store.models import Product, OrderItem, Order
 
 def say_hello(request):
-    queryset = Order.objects.select_related('customer').order_by('placed_at')[:5]
+    # result = Product.objects.aggregate(Count('id'))
+    result = Product.objects.aggregate(count = Count('id'), min_price = (Min('unit_price')))
 
 
-    return render(request, 'hello.html', {'name' : 'Amandeep', 'orders' : list(queryset)})
+    return render(request, 'hello.html', {'name' : 'Amandeep', 'result' : result})
     
