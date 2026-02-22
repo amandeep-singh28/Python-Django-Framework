a0d139221b9f3996cb3a7a932c2869e3e08dffb2
Amandeep Singh <amandeep87557@gmail.com>
2026-02-22 20:11:59 +0530

    Expression Wrapper Class Implementation

--- Django/playground/views.py
+++ Django/playground/views.py
@@ -6,17 +6,14 @@ from django.db.models.aggregates import Count, Min, Max, Sum, Avg
 from django.db.models import Value
 from django.db.models import Func
 from django.db.models.functions import Concat
+from django.db.models import ExpressionWrapper, DecimalField
 from store.models import Product, OrderItem, Order, Customer
 
 def say_hello(request):
     
-    queryset = Customer.objects.annotate(
-        full_name = Func(F('first_name'), Value(' '),
-                         F('last_name'), function = 'CONCAT')
-    )
-    queryset = Customer.objects.annotate(
-        full_name = Concat(F('first_name'), Value(' '),
-                           F('last_name'))
+    queryset = Product.objects.annotate(
+        # discounted_price = ExpressionWrapper(F('unit_price') * 0.82, output_field = DecimalField())
+        discounted_price = F('unit_price') * 0.82
     )
 
     return render(request, 'hello.html', {'name' : 'Amandeep', 'result' : list(queryset)})
